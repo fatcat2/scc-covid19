cf1640ff5b144643ac61e959d8ff5b65b45ffd3b
Ryan Chen <ryanjchen2@gmail.com>
2020-03-19 23:48:55 -0700

    finished frontend

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
 import os
+from collections import defaultdict
 
-from quart import Quart, jsonify, send_from_directory
+from quart import Quart, jsonify, send_from_directory, render_template
 import httpx
 
-app = Quart(__name__, static_folder='home/build')
+app = Quart(__name__, static_folder='home/build/static', template_folder="home/build")
 
 @app.route("/data")
 async def data_route():
@@ -18,14 +19,56 @@ async def data_route():
 
     return jsonify(ret_data)
 
+@app.route("/data/new_cases")
+async def data_new_cases_route():
+    async with httpx.AsyncClient() as client:
+        data = await client.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQCHQh-DwbQf1-rAq_zk77I5-0LpBidpkuYW8LbP0JxYwFxMV6YHD4NNSTlclWIUE7tHNY9O-fSU-mb/pub?gid=0&single=true&output=csv")
+
+    split_data = csv.reader(data.text.split("\n"))
+
+    proc_data = defaultdict(int)
+
+    for row in split_data:
+        if row[0] == "date":
+            continue
+        proc_data[row[0]] = proc_data[row[0]] + 1
+
+    ret_data = [{"date": key, "new_cases": proc_data[key]} for key in proc_data.keys()]
+
+    print(ret_data)
+
+    return jsonify(ret_data)
+
+@app.route("/data/cases")
+async def data_cases_route():
+    async with httpx.AsyncClient() as client:
+        data = await client.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQCHQh-DwbQf1-rAq_zk77I5-0LpBidpkuYW8LbP0JxYwFxMV6YHD4NNSTlclWIUE7tHNY9O-fSU-mb/pub?gid=0&single=true&output=csv")
+
+    split_data = csv.reader(data.text.split("\n"))
+
+    proc_data = defaultdict(int)
+
+    total = 0
+
+    for row in split_data:
+        if row[0] == "date":
+            continue
+        proc_data[row[0]] = proc_data[row[0]] + 1
+    
+    ret_data = []
+
+    for key in proc_data.keys():
+        total += proc_data[key]
+        ret_data.append({"date": key, "cases": total})
+
+    print(ret_data)
+
+    return jsonify(ret_data)
+
 # Serve React App
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-async def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return await send_from_directory(app.static_folder, path)
-    else:
-        return await send_from_directory(app.static_folder, 'index.html')
+@app.route("/")
+async def serve():
+    return await render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
